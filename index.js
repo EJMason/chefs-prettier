a975bd8b3a17064821481fd32c70018806942ffc
EJ Mason <ej-mason@pluralsight.com>
2019-11-13 14:10:56 -0700

    :package: Add all rules with defaults

---
+++ index.js
@@ -0,0 +1,25 @@
+//https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 120,
+  tabWidth: 4,
+  tabs: false,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+
+  //defaults
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css"
+  // parser? inferred
+};
